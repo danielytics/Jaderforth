abd553125e7818cf86d2eb601f451bfc60a85d23
Cody Brocious <cody.brocious@gmail.com>
2013-11-02 22:38:08 -0400

    Added some more base functionality.

--- src/jaderforth/core.clj
+++ src/jaderforth/core.clj
@@ -3,8 +3,10 @@
 
 (use '[clojure.core.match :only (match)])
 
-(defn parse [code]
-  (reverse (into () (clojure.string/split code #"\s+"))))
+(defn vec-list [vec] (reverse (into () vec)))
+
+(defn tokenize [code]
+  (vec-list (clojure.string/split code #"\s+")))
 
 (defn wordify [code]
   (defn sub [code words word-stack comment-depth]
@@ -19,7 +21,21 @@
       [_ ")"]   (recur (rest code) (append-token (first word-stack) ")") word-stack (- comment-depth 1))
       [_ nil]   words
       :else (recur (rest code) (append-token (first word-stack) (first code)) word-stack 0)))
-  (sub code {} '("main") 0))
+  (sub code {"main" [:word]} '("main") 0))
+
+(defn strip-comments [words] words)
+
+(defn split-words [words]
+  (reduce
+    (fn [[words macros] [k v]]
+      (match v
+        [:word & r] [(assoc words k r) macros]
+        [:macro & r] [words (assoc macros k r)]))
+    [{} {}]
+    words))
+
+(defn run-macros [words]
+  (split-words (strip-comments words)))
 
 (defn -main [& args]
-  (wordify (parse ": someword ( foo bar ) baz :m test ( a b ) a b + ; hax ;\n5 6 test")))
+  (run-macros (wordify (tokenize ": someword ( foo bar ) baz :m test ( a b ) a b + ; hax ;\n5 6 test"))))
